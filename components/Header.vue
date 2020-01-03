1df6dc2a27ccb17cfe5d7a7473368d34b0164c1b
YunDongHyeon <ydh2338@naver.com>
2020-01-03 14:06:59 +0900

    add background img

---
+++ components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <div id="familysite">
+        </div>
+        <div id="background">
+            <div class="container">
+                LOL Chess clone coding
+                <ul>
+                    <li>ff1</li>
+                    <li>ff2</li>
+                    <li>ff3</li>
+                    <li>ff4</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name : "header",
+    data(){
+        return {
+            
+        }
+    }
+}
+</script>
+
+<style>
+#background{
+    background-image: url("~static/image/background-banner.png");
+    background-position: center center;
+    height: 200px;
+}
+
+li{
+    display: inline;
+}
+</style>
