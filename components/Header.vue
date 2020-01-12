1a03b0b0d33e8be0ee6c5fec286ff34dfc0bd178
YunDongHyeon <ydh2338@naver.com>
2020-01-12 23:17:13 +0900

    menu list

--- components/Header.vue
+++ components/Header.vue
@@ -4,13 +4,19 @@
         </div>
         <div id="background">
             <div class="container">
-                LOL Chess clone coding
-                <ul>
-                    <li>ff1</li>
-                    <li>ff2</li>
-                    <li>ff3</li>
-                    <li>ff4</li>
-                </ul>
+                <div id="header-title">
+                    <img id="header-icon" src="~static/image/lolchessicon.png"/>
+                    <h1>LOL Chess clone coding</h1>    
+                </div>
+                <div id="direct-link">
+                    <ul>
+                        <li>가이드</li>
+                        <li>추천메타</li>
+                        <li>아이템</li>
+                        <li>배치율</li>
+                        <li>시뮬레이터</li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
@@ -28,13 +34,46 @@ export default {
 </script>
 
 <style>
+#familysite{
+    height: 40px;
+    background-color: #3a3f4c;
+}
+#familysite>li{
+    height: 40px;
+}
 #background{
     background-image: url("~static/image/background-banner.png");
-    background-position: center center;
+    background-position-x: right;
     height: 200px;
 }
+#header-title{
+    padding-top:60px;
+    height: 160px;
+}
 
+#header-title > h1{
+    height: 160px;
+}
+#direct-link{
+    height: 40px;
+}
+#header-icon{
+    float: left;
+    width: 3rem;
+}
+h1{
+    color: white;
+}
+ul{
+    margin: 0;
+    padding: 0;
+}
 li{
     display: inline;
+    margin-right: 10px;
+    color: #d6d6d6;
+}
+li:hover{
+    border-bottom: 4px solid white;
 }
 </style>
